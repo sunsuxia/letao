70b8317522e770ebea680b2a85036312b74862ad
sunsuxia <2567716270@qq.com>
2017-11-29 19:10:36 +0800

    我完成了进度条

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -1,3 +1,17 @@
-/**
- * Created by h on 2017/11/29.
- */
+//进度条功能
+//首先关闭进度环
+NProgress.configure({
+    showSpinner: false
+});
+//全局ajax事件之开启进度条
+$(document).ajaxStart(function () {
+    //开始进度条
+    NProgress.start();
+});
+//全局ajax事件之关闭进度条
+$(document).ajaxStop(function () {
+    //结束进度条
+    setTimeout(function () {
+        NProgress.done();
+    }, 500);
+});
